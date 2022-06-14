f73b47e65c894c7ef43141a8ae211ea55354a96e
desmondharris <desmondktharris@gmail.com>
2022-06-14 11:39:27 -0500

    working, except for issues with certain categorical data

--- ClassificationTree.py
+++ ClassificationTree.py
@@ -1,6 +1,8 @@
+from msilib.schema import Class
 import numpy as np
 import math
 import pandas as pd
+import seaborn as sbs
 from statistics import mode
 class Leaf: 
     def __init__(self, outputs):
@@ -16,7 +18,8 @@ def find_avgs(sorted):
 #standard shannon entropy equation
 def entropy(vect):
     entropy = 0
-    for i in set(vect.tolist()):
+    # Don't forget to convert output array into numpy array.
+    for i in set(vect):
         prob = len(np.where(vect == i)[0]) / len(vect)
         entropy = entropy - (prob * math.log(prob, 2))
     return entropy
@@ -31,10 +34,12 @@ def categorical_information_gain(vect, classes):
     pass
 
 class Node:
-    def __init__(self, data, outputs, vars):
+    def __init__(self, data, outputs, vars, sf = [], path = ['rt']):
         self.data = data
         self.outputs = outputs
         self.vars = vars
+        sf.append(path)
+        self.path = sf
         information_gains = []
         #go through each eligible variable and calculate the entropy.
         for i in self.vars:
@@ -43,6 +48,7 @@ class Node:
             else:
                 information_gains.append(self.continuous_find_split(i)[0])
         self.split = self.vars[information_gains.index(max(information_gains))]
+    
         #remove the current variable from this branch of the tree.
         self.vars.remove(self.split)
 
@@ -51,11 +57,12 @@ class Node:
             # because this model requires categorical data to be given as a string, we use a dictionary to handle the varied amount of categories
             # a sample can be classified as. the name of the class is the key, and the Node/Leaf it leads to is the value.
             self.splitdict = {}
+            h = set(data[:, self.split])
             for i in set(data[:, self.split]):
-                if len(data[np.where(data[:, self.split] == i)]) > 25:
-                    self.splitdict[i] = Node(data[np.where(data[:, self.split] == i)], outputs[np.where(data[:, self.split] == i)], self.vars)    
+                if entropy(outputs[np.where(data[:, self.split] == i)]) > 1 and len(vars) > 1:
+                    self.splitdict[i] = Node(data[np.where(data[:, self.split] == i)], outputs[np.where(data[:, self.split] == i)], self.vars, self.path, 'ct')    
                 else: 
-                    self.splitdict[i] = outputs[np.where(data[:, self.split] == i)]
+                    self.splitdict[i] = Leaf(outputs[np.where(data[:, self.split] == i)])
         else:
             # handle a continuous variable
             # the point where the decision tree splits, i.e "age <= 16?"
@@ -66,12 +73,12 @@ class Node:
             rightoutputs = outputs[np.where(data[:, self.split] > self.cutoff)]
             
 
-            if len(leftdata) > 50:
-                self.left = Node(leftdata, leftoutputs, self.vars)
+            if entropy(leftoutputs) > 1 and len(vars) > 1:
+                self.left = Node(leftdata, leftoutputs, self.vars, self.path, 'r')
             else:
                 self.left = Leaf(leftoutputs)
-            if len(rightdata) > 50:
-                self.right = Node(rightdata, rightoutputs, self.vars)
+            if entropy(rightoutputs) > 1 and len(vars) > 1:
+                self.right = Node(rightdata, rightoutputs, self.vars, self.path, 'l')
             else:
                 self.right = Leaf(rightoutputs)
                
@@ -83,6 +90,7 @@ class Node:
         disorders = []
         for cnt, i in enumerate(avgs):
             left = self.data[np.where(self.data[:, idx] <= i)]
+            g = np.where(self.data[:, idx] <= i)[0][0]
             leftoutputs = self.outputs[np.where(self.data[:, idx] <= i)]
             right = self.data[np.where(self.data[:, idx] > i)]
             rightoutputs = self.outputs[np.where(self.data[:, idx] > i)]
@@ -117,15 +125,30 @@ class ClassificationTree:
                     current = current.left
         return current.output
 
+    def findAccuracy(self,testdata, testoutputs):
+        correct = 0
+        for i, j in zip(testdata, testoutputs):
+            if self.predict(i) == j:
+                correct += 1
+        return correct / len(testdata)
 
-""" testdata = np.array([[1,3,4,'C'],
-                      [4,6,1,'C'],
-                      [1,6,9, 'A'],
-                      [5,3,1, 'A']], dtype= 'object')
-testoutputs = np.array([1,1,0,0]) """
-data = pd.read_excel("C:/Users/dsm84762/Desktop/Data.xlsx",usecols="A:AH", nrows= 633).to_numpy()
-outputs_b = pd.read_excel("C:/Users/dsm84762/Desktop/Data.xlsx",usecols="AS:AS", nrows = 633).to_numpy()
-outputs = np.array([x for xs in outputs_b for x in xs])
-test = ClassificationTree(data, outputs)
-g = np.array([1] * 34)
-print(test.predict(g))
+
+
+"""trainingdata = pd.read_csv(r"C:/Users/dsm84762/Desktop/Python Code/knntestdata/KNNAlgorithmDataset.csv", nrows = 500, usecols= range(2,32)).to_numpy()
+trainingoutput = pd.read_csv(r"C:/Users/dsm84762/Desktop/Python Code/knntestdata/KNNAlgorithmDataset.csv", nrows = 500, usecols = [1] ).to_numpy()
+trainingoutput = np.array([x for xs in trainingoutput for x in xs])
+
+testdata = pd.read_csv(r"C:/Users/dsm84762/Desktop/Python Code/knntestdata/KNNAlgorithmDataset.csv", skiprows = 500, usecols= range(2,32)).to_numpy()
+testoutput = pd.read_csv(r"C:/Users/dsm84762/Desktop/Python Code/knntestdata/KNNAlgorithmDataset.csv", skiprows = 500, usecols = [1] ).to_numpy()
+"""
+# Extract example dataset from GitHub repository
+Example_TO = pd.read_csv('https://raw.githubusercontent.com/desmondharris/MachineLearningPractice/main/Datasets/car_evaluation.csv', nrows = 400, usecols = [6]).to_numpy()
+Example_TO = np.array([item for sublist in Example_TO for item in sublist])
+Example_TD = pd.read_csv('https://raw.githubusercontent.com/desmondharris/MachineLearningPractice/main/Datasets/car_evaluation.csv', nrows = 400, usecols = range(6)).to_numpy()
+
+Example_TrO = pd.read_csv('https://raw.githubusercontent.com/desmondharris/MachineLearningPractice/main/Datasets/car_evaluation.csv', skiprows = 400, usecols = [6]).to_numpy()
+Example_TrO = np.array([item for sublist in Example_TrO for item in sublist])
+Example_TrD = pd.read_csv('https://raw.githubusercontent.com/desmondharris/MachineLearningPractice/main/Datasets/car_evaluation.csv', skiprows = 400, usecols = range(6)).to_numpy()
+
+Example_Tree = ClassificationTree(Example_TrD, Example_TrO)
+print(Example_Tree.findAccuracy(Example_TD,Example_TO))
